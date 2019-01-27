8bc56ba75e036219489fa88f41c1628db5c9919e
Alec.Ray <ajray@uvm.edu>
2019-01-26 19:40:37 -0500

    initial commit, test scene, basic movement and dialogue functions

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent playerAgent;
+    private bool hasInteracted;
+
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent)
+    {
+        hasInteracted = false;
+        this.playerAgent = playerAgent;
+        playerAgent.stoppingDistance = 3f;
+        // Set the player's destination = to this Interactable's position
+        playerAgent.destination = this.transform.position;
+    }
+
+    void Update()
+    {
+        // Check if hasn't interacted
+        // And we have a player agent
+        // And a path is not pending
+        if (!hasInteracted && playerAgent != null && !playerAgent.pathPending)
+        {
+            if (playerAgent.remainingDistance <= playerAgent.stoppingDistance)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    public virtual void Interact()
+    {
+        // interacting w/ base class not NPC
+        Debug.Log("Interacting with base class.");
+    }
+}
---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour
+{
+    NavMeshAgent playerAgent;
+
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // If left mouse is down and pointer is over game object
+        if(Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Output raycast info to interactionInfo
+        RaycastHit interactionInfo;
+
+        // Hit an object with a raycast, store it as an interacted object so we 
+        // can do something with it later on
+
+        // If our ray hits
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            // Get game object of collider that our ray hit
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                // Get the interactable component of the interacted object
+                // and move to it
+                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+;            }
+            else // Player agent not interactable object
+            {
+                playerAgent.stoppingDistance = 0;
+                // Set player destination to the click point
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+
+}
